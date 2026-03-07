c9074a1ef44fb750332d343de01a2c81aa3831fb
navvod <priyashanakila99@gmail.com>
2026-03-07 19:19:46 +0530

    network error fixed

--- frontend/frontend/src/config/config.js
+++ frontend/frontend/src/config/config.js
@@ -1,7 +1,7 @@
 import { API_BASE as ENV_API_BASE } from '@env';
 
 // Use the ENV value if available, otherwise fallback to your current PC IP
-export const API_BASE = ENV_API_BASE || "http://192.168.8.199:8000/api";
+export const API_BASE = ENV_API_BASE || "http://172.20.10.5:8000/api";
 
 console.log("🔧 Config loaded. API_BASE:", API_BASE);
 
